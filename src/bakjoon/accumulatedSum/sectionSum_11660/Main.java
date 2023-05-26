fb445b02c3b2d14da925af04de1aed6a9cdd5ca1
kim3439 <kim3439@clipsoft.co.kr>
2023-05-26 10:48:13 +0900

    백준 단계별로 풀어보기 - 누적 합 - 구간 합 구하기 5_11660

---
+++ src/bakjoon/accumulatedSum/sectionSum_11660/Main.java
@@ -0,0 +1,48 @@
+package bakjoon.accumulatedSum.sectionSum_11660;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] arrN = new int[N+1][N+1];
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			for(int j = 1; j <= N; j++) {
+				arrN[i][j] = Integer.parseInt(st.nextToken());
+			}			
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < M; i++) {
+			int sum = 0;
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			for(int j = x1; j <= x2; j++) {
+				for(int k = y1; k <= y2; k++) {
+					sum += arrN[j][k];
+				}
+			}
+			
+			sb.append(sum).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
