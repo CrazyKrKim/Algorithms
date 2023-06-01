6ebccb5856c8b48dddfcd99fc638f648bbe2e58a
CrazyKrKim <rlatjd789@naver.com>
2023-06-01 16:59:37 +0900

    백준 단계별로 풀어보기 - 분할정복 - 이항계수3_11401

---
+++ src/bakjoon/splitConquest/binomialCoefficient_11401/Main.java
@@ -0,0 +1,49 @@
+package bakjoon.splitConquest.binomialCoefficient_11401;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {	
+	static long P = 1000000007;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		long N = Long.parseLong(st.nextToken());
+		long K = Long.parseLong(st.nextToken());
+		
+		long numer = factorial(N);
+		
+		long denom = factorial(K) * factorial(N - K) % P;
+		
+		System.out.println(numer * pow(denom, P - 2) % P);
+	}
+	
+	static long factorial(long N) {
+		long fac = 1L;
+		
+		while(N > 1) {
+			fac = (fac * N) % P;
+			N--;
+		}
+		
+		return fac;
+	}
+	
+	static long pow(long base, long expo) {
+		if(expo == 1) {
+			return base % P;
+		}
+		
+		long temp = pow(base, expo / 2);
+		
+		if(expo % 2 == 1) {
+			return (temp * temp % P) * base % P;
+		}
+		
+		return temp * temp % P;
+	}
+}
