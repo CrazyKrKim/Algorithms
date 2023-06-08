147c1677796791d2a3eaa69b612720c0a5ff60a6
CrazyKrKim <rlatjd789@naver.com>
2023-06-08 11:02:49 +0900

    백준 단계별로 풀어보기 - 분할정복 - 행렬곱셈_2740

---
+++ src/bakjoon/splitConquest/processionMultiple_2740/Main2.java
@@ -0,0 +1,59 @@
+package bakjoon.splitConquest.processionMultiple_2740;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main2 {	
+	static int[][] firstArr;
+	static int[][] secondArr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int firstY = Integer.parseInt(st.nextToken());
+		int firstX = Integer.parseInt(st.nextToken());
+		
+		firstArr = new int[firstY][firstX];
+		
+		for(int i = 0; i < firstY; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			for(int j = 0; j < firstX; j++) {				
+				firstArr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int secondY = Integer.parseInt(st.nextToken());
+		int secondX = Integer.parseInt(st.nextToken());
+		
+		secondArr = new int[secondY][secondX];
+		
+		for(int i = 0; i < secondY; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			for(int j = 0; j < secondX; j++) {				
+				secondArr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < firstY; i++) {
+			for(int j = 0; j < secondX; j++) {
+				int sum = 0;
+				for(int k = 0; k < firstX; k++) {
+					sum += firstArr[i][k] * secondArr[k][j];
+				}
+
+				sb.append(sum).append(" ");
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
