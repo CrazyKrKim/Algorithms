2676399298da41b2c0102b75d35c6f898991ef5e
CrazyKrKim <rlatjd789@naver.com>
2023-06-23 15:48:14 +0900

    백준 단계별로 풀어보기 - 우선순위 큐 - 최소 힙_1927

---
+++ src/bakjoon/priorityQueue/minHeap_1927/Main.java
@@ -0,0 +1,33 @@
+package bakjoon.priorityQueue.minHeap_1927;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.io.IOException;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x == 0) {
+				if(priorityQueue.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(priorityQueue.poll()).append("\n");
+				}
+			} else {
+				priorityQueue.offer(x);
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
